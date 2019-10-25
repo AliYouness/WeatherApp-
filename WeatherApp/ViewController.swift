86eb0257c979327d9bbda8ef4be4c5d3f6f1851d
Ali.Youness <ali.youness@live.com>
2019-10-25 11:19:37 -0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Ali Youness on 10/25/19.
+//  Copyright © 2019 Ali Youness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
